da9617da152bba3d30be22c3a8c2c43a2b510fe5
ChasGPT <charles4865@gmail.com>
2026-04-24 14:25:52 +0900

    Create giscus.js

---
+++ board/giscus.js
@@ -0,0 +1,18 @@
+const giscusScript = document.createElement("script");
+
+giscusScript.src = "https://giscus.app/client.js";
+giscusScript.setAttribute("data-repo", "chagpts/start");
+giscusScript.setAttribute("data-repo-id", "R_kgDOSGxCnw");
+giscusScript.setAttribute("data-category", "Comments");
+giscusScript.setAttribute("data-category-id", "DIC_kwDOSGxCn84C7kv5");
+giscusScript.setAttribute("data-mapping", "pathname");
+giscusScript.setAttribute("data-strict", "0");
+giscusScript.setAttribute("data-reactions-enabled", "1");
+giscusScript.setAttribute("data-emit-metadata", "0");
+giscusScript.setAttribute("data-input-position", "bottom");
+giscusScript.setAttribute("data-theme", "light");
+giscusScript.setAttribute("data-lang", "ko");
+giscusScript.setAttribute("crossorigin", "anonymous");
+giscusScript.async = true;
+
+document.body.appendChild(giscusScript);
